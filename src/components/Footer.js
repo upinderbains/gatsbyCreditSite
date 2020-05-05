a09664ff857a8c19e633e10ac6b5fe9f1ff27683
pinder <upinder350@gmail.com>
2020-05-05 17:46:32 -0400

    fixed footer icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,9 +15,16 @@ const StyledFooter = styled.footer`
   padding: 3rem;
 `
 const SocialLinks = styled.a`
+  padding: 10px;
   svg {
-    width: 30px;
-    height: 30px;
+    fill: var(--color-grey-light-1);
+    width: 20px;
+    height: 20px;
+    transition: all .5s;
+    &:hover,
+    &:focus {
+      fill: var(--color-primary)
+    }
   }
 `
 
@@ -57,14 +64,18 @@ const Footer = () => {
                 fade
                 key={index}
                 to={link.path}
-                activeStyle={{ color: "var(--color-primary)" }}
+                activeStyle={{
+                  color: "var(--color-primary)",
+                }}
               >
+                {" "}
                 {link.text}
               </AniLink>
             )
           })}
         </NavLinks>
         <div>
+          {" "}
           {socialLinks.map((link, index) => {
             return (
               <SocialLinks
@@ -73,16 +84,17 @@ const Footer = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                {link.icon}
+                {" "}
+                {link.icon}{" "}
               </SocialLinks>
             )
-          })}
+          })}{" "}
         </div>
       </StyledContainer>
 
       <StyledCopyright>
-        copyright &copy; new company {new Date().getFullYear()} all rights
-        reserved
+        copyright & copy; new company {new Date().getFullYear()}
+        all rights reserved
       </StyledCopyright>
     </StyledFooter>
   )
--- src/constants/socialIcons.js
+++ src/constants/socialIcons.js
@@ -2,14 +2,25 @@ import React from "react"
 
 import IconFacebook from "../icons/facebook"
 import IconInstagram from "../icons/instagram"
+import IconLocation from '../icons/location'
+import IconTwitter from '../icons/twitter'
 
-export default [
-  {
-    icon: <IconFacebook />,
+
+export default [{
+    icon: < IconFacebook / > ,
     url: "https://facebook.com"
   },
   {
-    icon: <IconInstagram />,
+    icon: < IconInstagram / > ,
     url: "https://instagram.com"
-  }
+  },
+  {
+    icon: < IconTwitter / > ,
+    url: "https://twitter.com"
+  },
+  {
+    icon: < IconLocation / > ,
+    url: "https://instagram.com"
+  },
+
 ]
